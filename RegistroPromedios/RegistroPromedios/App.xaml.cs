992ef9cca380b1016231b0608cee66433b9b000e
lcernav242-coder <lcernav_24_2@unc.edu.pe>
2026-06-21 22:54:25 -0500

    Add files via upload

---
+++ RegistroPromedios/RegistroPromedios/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace RegistroPromedios
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
