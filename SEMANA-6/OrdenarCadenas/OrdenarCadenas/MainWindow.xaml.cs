54f5539596d3d00df04f0b62bf79251691e7eb61
Luis Cerna <TU_CORREO_DE_GITHUB>
2026-07-10 08:46:33 -0500

    Código completo Ordenar cadena

--- SEMANA-6/OrdenarCadenas/OrdenarCadenas/MainWindow.xaml.cs
+++ SEMANA-6/OrdenarCadenas/OrdenarCadenas/MainWindow.xaml.cs
@@ -1,24 +1,63 @@
-﻿using System.Text;
+﻿using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace OrdenarCadenas
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnListar_Click(object sender, RoutedEventArgs e)
+        {
+            lstTodos.Items.Clear();
+
+            string cadena = txtCadenaOriginal.Text.Trim();
+
+            if (string.IsNullOrEmpty(cadena))
+            {
+                MessageBox.Show("Por favor, asegúrese de que haya nombres en la caja de texto.");
+                return;
+            }
+
+            string[] nombres = cadena.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string nombre in nombres)
+            {
+                lstTodos.Items.Add(nombre);
+            }
+
+            txtContadorTotal.Text = nombres.Length.ToString();
+        }
+
+        private void btnPasar_Click(object sender, RoutedEventArgs e)
+        {
+            lstFiltrados.Items.Clear();
+
+            string letra = txtLetraFiltro.Text.Trim();
+
+            if (string.IsNullOrEmpty(letra))
+            {
+                MessageBox.Show("Por favor, ingrese la inicial que desea buscar.");
+                return;
+            }
+
+            int contador = 0;
+
+            foreach (var item in lstTodos.Items)
+            {
+                string nombre = item.ToString();
+
+                if (nombre.StartsWith(letra, StringComparison.OrdinalIgnoreCase))
+                {
+                    lstFiltrados.Items.Add(nombre);
+                    contador++;
+                }
+            }
+
+            txtContadorFiltrados.Text = contador.ToString();
         }
     }
 }
