d2cbb4d12c6c9551c74d31c48e771ce2cbcc0b39
Luis Cerna <TU_CORREO_DE_GITHUB>
2026-07-20 09:59:09 -0500

    Trabajando Menu Contextual

---
+++ SEMANA 08/MenusContextuales/MenuPrincipal.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MenusContextuales
+{
+    /// <summary>
+    /// Lógica de interacción para MenuPrincipal.xaml
+    /// </summary>
+    public partial class MenuPrincipal : Window
+    {
+        public MenuPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
