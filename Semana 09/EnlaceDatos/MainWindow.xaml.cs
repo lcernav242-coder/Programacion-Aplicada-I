6b9f4bbffe3a3077062b0742d3b8a446b6dcf3a2
Luis Cerna <TU_CORREO_DE_GITHUB>
2026-08-03 08:59:29 -0500

    Data Binding

---
+++ Semana 09/EnlaceDatos/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EnlaceDatos
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Producto producto;
+        public MainWindow()
+        {
+            InitializeComponent();
+            producto = new Producto();
+            producto.Nombre = "Café";
+            this.DataContext = producto;
+        }
+
+        private void btnMostrar_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"El precio = {producto.Precio}");
+        }
+    }
+}
---
+++ Semana 09/EnlaceDatos/Producto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnlaceDatos
+{
+    internal class Producto
+    {
+        public string Nombre { get; set; }
+        public int Precio { get; set; }
+    }
+}
